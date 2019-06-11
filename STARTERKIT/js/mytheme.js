9918cd7b26354ce0bf7ccca83c7918582c3d67d8
jmfegu <jmfegu@gmail.com>
2019-06-11 13:38:23 +0200

    #NA: NEW STARTERKIT

--- STARTERKIT/js/mytheme.js
+++ STARTERKIT/js/mytheme.js
@@ -8,8 +8,6 @@
 (function ($) {
   Drupal.behaviors.mythemeTheme = {
     attach: function (context) {
-
-      
     }
   };
 })(jQuery);
---
+++ STARTERKIT/js/utils/in-view.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * In view example usage
+ */
+
+'use strict';
+
+(function (Drupal, $) {
+  $(document).ready(function () {
+
+    // $('selector:in-viewport').addClass('is-visible');
+
+  });
+})(Drupal, jQuery);
